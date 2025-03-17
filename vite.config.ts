db8a2c48813e036b04551c397d531e64a096f783
Andrzej Wojtowicz <andrzej.wojtowicz@example.com>
2025-03-17 09:12:29 +0100

    Update Vite config for proper deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
     minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
