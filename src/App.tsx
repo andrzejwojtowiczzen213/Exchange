454b9790d0f438835ce230e662aa522321dc390d
Andrzej Wojtowicz <andrzej.wojtowicz@example.com>
2025-03-17 08:41:47 +0100

    Fix routing for Vercel deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Container from './components/Container';
 import Checkout from './components/Checkout';
 import Status from './components/Status';
